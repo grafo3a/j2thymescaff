c063d8cc25a207a75be2ef27d9fc802c9ac416c8
ApasaJB <jb.apasa@gmail.com>
2026-06-23 15:58:49 +0200

    feat: add a launcher class [J2thymescaffLauncher.java]

---
+++ src/net/apasajb/j2thymescaff/J2thymescaffLauncher.java
@@ -0,0 +1,12 @@
+package net.apasajb.j2thymescaff;
+
+
+public class J2thymescaffLauncher {
+	
+	public static void main(String[] args) {}
+	
+	// This private constructor prevents instantiation
+	private J2thymescaffLauncher() {
+		throw new IllegalStateException("Utility class");
+	}
+}
