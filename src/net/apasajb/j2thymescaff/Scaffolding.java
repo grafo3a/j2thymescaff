70161b147c0dc4fa4e380b63c3830886aa21eb1e
ApasaJB <jb.apasa@gmail.com>
2026-06-25 22:53:24 +0200

    feat: add a method [isValidJpaEntity]

--- src/net/apasajb/j2thymescaff/Scaffolding.java
+++ src/net/apasajb/j2thymescaff/Scaffolding.java
@@ -3,4 +3,12 @@ package net.apasajb.j2thymescaff;
 
 public class Scaffolding {
 	
+	public Boolean isValidJpaEntity(Object myClass) {
+		
+		boolean isEntity = false;
+		
+		//isEntity = myClass.getClass().isAnnotationPresent(jakarta.persistence.Entity.class);
+		
+		return isEntity;
+	}
 }
