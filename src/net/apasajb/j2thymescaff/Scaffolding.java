495f7a63a50d775f91bcc50e7d1ae5695be6416c
ApasaJB <jb.apasa@gmail.com>
2026-06-26 17:31:31 +0200

    refactor: update logic in a method [isValidJpaEntity]

--- src/net/apasajb/j2thymescaff/Scaffolding.java
+++ src/net/apasajb/j2thymescaff/Scaffolding.java
@@ -1,6 +1,7 @@
 package net.apasajb.j2thymescaff;
 
 import jakarta.persistence.Entity;
+import org.tinylog.Logger;
 
 
 public class Scaffolding {
@@ -8,8 +9,12 @@ public class Scaffolding {
 	public Boolean isValidJpaEntity(Entity myClass) {
 		
 		boolean isClassValidEntity = false;
+		String myClassName = myClass.name();
 		
-		//isEntity = myClass.getClass().isAnnotationPresent(jakarta.persistence.Entity.class);
+		Logger.info("\nDebut verif si la classe " + myClassName + " est une entit/e valide.");
+		isClassValidEntity = myClass.getClass().isAnnotationPresent(jakarta.persistence.Entity.class);
+		
+		Logger.info("Fin verif de la classe " + myClassName + ".");
 		
 		return isClassValidEntity;
 	}
