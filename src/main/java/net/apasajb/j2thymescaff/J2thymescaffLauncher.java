2418efafd8e85b43f5c14ff36bf97d39615ceb25
ApasaJB <jb.apasa@gmail.com>
2026-08-20 23:42:01 +0200

    feat: add a method [getListeAttributs()]

--- src/main/java/net/apasajb/j2thymescaff/J2thymescaffLauncher.java
+++ src/main/java/net/apasajb/j2thymescaff/J2thymescaffLauncher.java
@@ -1,5 +1,7 @@
 package net.apasajb.j2thymescaff;
 
+import java.lang.reflect.Field;
+
 import org.tinylog.Logger;
 
 
@@ -7,11 +9,34 @@ public class J2thymescaffLauncher {
 	
 	public static void main(String[] args) {
 		Logger.info("Debut du demarrage de l'outil J2thymescaff.");
+		
+		getListeAttributs();
 	}
 	
 	
 	// This private constructor prevents instantiation
 	private J2thymescaffLauncher() {
 		throw new IllegalStateException("Error. This utility class cannot be instantiated.");
+	}
+	
+	public static void getListeAttributs() {
+		//String userDir = System.getProperty("user.dir");
+		//System.out.println("\n ==== pwd: " + userDir + "/");
+		
+		Class<?> entityClass = Compte.class;
+		Field[] fields = entityClass.getDeclaredFields();
+		String attribut = "";
+		
+		Logger.info("");
+		
+		for (Field field : fields) {
+			// Optional: Skip fields annotated with @Transient
+			if (field.isAnnotationPresent(jakarta.persistence.Transient.class)) {
+				continue;
+			}
+			
+			attribut = "*Field name: " + field.getName() + " | Type: " + field.getType().getSimpleName();
+			Logger.info(attribut);
+		}
 	}
 }
