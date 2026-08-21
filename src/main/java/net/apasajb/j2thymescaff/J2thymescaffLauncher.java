b1d49e85bd255cf00221ef35eabf6fea95fccc42
ApasaJB <jb.apasa@gmail.com>
2026-08-21 17:06:40 +0200

    refactor: move a method [getListeAttributs()] to Scaffolding.java

--- src/main/java/net/apasajb/j2thymescaff/J2thymescaffLauncher.java
+++ src/main/java/net/apasajb/j2thymescaff/J2thymescaffLauncher.java
@@ -1,42 +1,21 @@
 package net.apasajb.j2thymescaff;
 
-import java.lang.reflect.Field;
-
 import org.tinylog.Logger;
 
 
 public class J2thymescaffLauncher {
 	
 	public static void main(String[] args) {
-		Logger.info("Debut du demarrage de l'outil J2thymescaff.");
 		
-		getListeAttributs();
+		Logger.info("\n==== Debut du demarrage de l'outil J2thymescaff ====");
+		
+		Scaffolding scaffObject = new Scaffolding();
+		scaffObject.getListeAttributs();
 	}
 	
 	
 	// This private constructor prevents instantiation
 	private J2thymescaffLauncher() {
 		throw new IllegalStateException("Error. This utility class cannot be instantiated.");
-	}
-	
-	public static void getListeAttributs() {
-		//String userDir = System.getProperty("user.dir");
-		//System.out.println("\n ==== pwd: " + userDir + "/");
-		
-		Class<?> entityClass = Compte.class;
-		Field[] fields = entityClass.getDeclaredFields();
-		String attribut = "";
-		
-		Logger.info("");
-		
-		for (Field field : fields) {
-			// Optional: Skip fields annotated with @Transient
-			if (field.isAnnotationPresent(jakarta.persistence.Transient.class)) {
-				continue;
-			}
-			
-			attribut = "*Field name: " + field.getName() + " | Type: " + field.getType().getSimpleName();
-			Logger.info(attribut);
-		}
 	}
 }
--- src/main/java/net/apasajb/j2thymescaff/Scaffolding.java
+++ src/main/java/net/apasajb/j2thymescaff/Scaffolding.java
@@ -1,6 +1,9 @@
 package net.apasajb.j2thymescaff;
 
 import jakarta.persistence.Entity;
+
+import java.lang.reflect.Field;
+
 import org.tinylog.Logger;
 
 
@@ -17,5 +20,27 @@ public class Scaffolding {
 		Logger.info("Fin verif de la classe " + myClassName + ".");
 		
 		return isClassValidEntity;
+	}
+	
+	
+	public void getListeAttributs() {
+		//String userDir = System.getProperty("user.dir");
+		//System.out.println("\n ==== pwd: " + userDir + "/");
+		
+		Class<?> entityClass = Compte.class;
+		Field[] fields = entityClass.getDeclaredFields();
+		String attribut = "";
+		
+		Logger.info("");
+		
+		for (Field field : fields) {
+			// Optional: Skip fields annotated with @Transient
+			if (field.isAnnotationPresent(jakarta.persistence.Transient.class)) {
+				continue;
+			}
+			
+			attribut = "*Field name: " + field.getName() + " | Type: " + field.getType().getSimpleName();
+			Logger.info(attribut);
+		}
 	}
 }
